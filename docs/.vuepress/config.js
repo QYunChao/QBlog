07a68d2ae4b3c7c1fb870c2b05c92a100e75854b
QYunChao <2509089625@qq.com>
2020-11-27 12:49:07 +0800

    change some directory structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,21 @@
-const nav = require('./nav');
-const sidebar = require('./sidebar');
+const nav = require("./nav");
+const sidebar = require("./sidebar");
 
 module.exports = {
-    title: '学无止，爱无涯',
-    base: '/QBlog/',
-    dest: 'dist',
-    description: 'Just playing around',
-    themeConfig: {
-        sidebarDepth: 2,
-        nav,
-        sidebar,
-        lastUpdated: "最后更新时间",
-        docsDir: 'docs',
-        repo: "https://qyunchao.github.io/QBlog"
-    },
-    extraWatchFiles: [
-        "./nav.js",
-        "./sidebar.js"
-    ],
-    markdown: {
-        lineNumbers: true
-    }
-}
+  title: "学无止，爱无涯",
+  base: "/QBlog/",
+  dest: "dist",
+  description: "Just playing around",
+  themeConfig: {
+    sidebarDepth: 2,
+    nav,
+    sidebar,
+    lastUpdated: "最后更新时间",
+    docsDir: "docs",
+    repo: "https://github.com/QYunChao/QBlog",
+  },
+  extraWatchFiles: ["./nav.js", "./sidebar.js"],
+  markdown: {
+    lineNumbers: true,
+  },
+};
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,41 +1,42 @@
 const project = [
-    {
-        title: "一个完整项目的搭建",
-        collapsable: false,
-        children: [
-            'firstProject/introduce',
-            'firstProject/start'
-        ]
-    }
+  {
+    title: "一个完整项目的搭建",
+    collapsable: true,
+    children: ["firstProject/introduce", "firstProject/start"],
+  },
 ];
 
 const web_front = [
-    {
-        title: "前端基础(HTML,JS,CSS)",
-        collapsable: false,
-        children: [
-            'base/base'
-        ]
-    }
+  {
+    title: "前端基础",
+    collapsable: true,
+    children: ["base/html", "base/js", "base/css"],
+  },
+  {
+    title: "你不知道的JS",
+    collapsable: true,
+    children: [
+      "JSyoudontknow/Part I",
+      "JSyoudontknow/Part II",
+      "JSyoudontknow/Part III",
+    ],
+  },
 ];
 
 const algorithm = [
-    {
-        title: "leetcode",
-        collapsable: false,
-        children: [
-            'leetcode/leetcode',
-            'leetcode/sort'
-        ]
-    },
+  {
+    title: "leetcode",
+    collapsable: true,
+    children: ["leetcode/leetcode", "leetcode/sort"],
+  },
 ];
 
 const design_pattern = {
-    title: "设计模式",
+  title: "设计模式",
 };
 
 module.exports = {
-    "/project/": project,
-    "/web-front/": web_front,
-    "/algorithm/": algorithm
-} 
+  "/project/": project,
+  "/web-front/": web_front,
+  "/algorithm/": algorithm,
+};
